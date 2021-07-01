6e2dbba2fd7ccb07493a056c837b7ca0567e8a7c
bakerv <80018640+bakerv@users.noreply.github.com>
2021-07-01 16:46:05 -0500

    first chart successful

--- javascript/app.js
+++ javascript/app.js
@@ -3,10 +3,10 @@
 let data = d3.json("data/samples.json").then((rawData) => {
     
     let bacteria = rawData.samples[0];
-
+    let lastElement = bacteria.sample_values.length -1;
     let bacteriaTrace = {
-        x: bacteria.sample_values,
-        y: bacteria.otu_ids,
+        x: bacteria.sample_values.reverse().slice(lastElement-11,lastElement-1),
+
         text: data.otu_labels,
         type:"bar",
         orientation:"h"
@@ -19,7 +19,7 @@ let data = d3.json("data/samples.json").then((rawData) => {
     let individualBacteria = [bacteriaTrace];
     Plotly.newPlot("bacteriaCounts", individualBacteria, bacteriaLayout)
 
-    console.log(data);
-    console.log(rawData.samples[0]);
+    console.log(rawData)
+    console.log(lastElement)
 });
 
