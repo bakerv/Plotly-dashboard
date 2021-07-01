eb7dcb1cb98dd2492e1dba3deb454c6c1d31c747
bakerv <80018640+bakerv@users.noreply.github.com>
2021-07-01 16:09:13 -0500

    successfully extracted and plotted data from inside the promise

--- javascript/app.js
+++ javascript/app.js
@@ -1,12 +1,25 @@
-d3.json("data/samples.json").then((rawData) => {
-    let data = rawData;
+
+
+let data = d3.json("data/samples.json").then((rawData) => {
+    
+    let bacteria = rawData.samples[0];
+
+    let bacteriaTrace = {
+        x: bacteria.sample_values,
+        y: bacteria.otu_ids,
+        text: data.otu_labels,
+        type:"bar",
+        orientation:"h"
+    };
+
+    let bacteriaLayout = {
+        title: "I hope this works!"
+    };
+
+    let individualBacteria = [bacteriaTrace];
+    Plotly.newPlot("bacteriaCounts", individualBacteria, bacteriaLayout)
+
     console.log(data);
-})
-// let bacteriaTrace = {
-//     x:,
-//     y:,
-//     text:,
-//     type:,
-//     orientation:
-// }
-plotly.newPlot("BbacteriaCounts", individualBacteria, bacteriaLayout)
+    console.log(rawData.samples[0]);
+});
+
