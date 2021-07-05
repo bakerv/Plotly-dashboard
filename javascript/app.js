3bf5ef5da861a7a70d5f0b8fd9728c688fa2593f
bakerv <80018640+bakerv@users.noreply.github.com>
2021-07-05 10:55:59 -0500

    demographics table sucessfully added

--- javascript/app.js
+++ javascript/app.js
@@ -6,22 +6,24 @@ let data = d3.json("data/samples.json").then((rawData) => {
     let lastElement = bacteria.sample_values.length -1;
 
     let individualMetadata = rawData.metadata[0];
+    
+
 
     // need to add interactive feature to adjust number of visible bars
-    let bacteriaTrace = {
+    let bacteriaTrace = [{
         x: bacteria.sample_values.reverse().slice(lastElement-11,lastElement-1),
 
         text: data.otu_labels,
         type:"bar",
         orientation:"h"
-    };
+    }];
     //need to improve layout
 
     let bacteriaLayout = {
         title: "I hope this works!"
     };
 
-    let bubbleTrace = {
+    let bubbleTrace = [{
 
         x:bacteria.otu_ids,
         y: bacteria.sample_values,
@@ -31,17 +33,30 @@ let data = d3.json("data/samples.json").then((rawData) => {
             size: bacteria.sample_values,
             color: bacteria.otu_ids,
         }
-    };
+    }];
 
     let bubbleLayout = {
         title: "This should also work"
     };
-    let individualBacteria = [bacteriaTrace];
-    let individualBubble = [bubbleTrace]
-    Plotly.newPlot("bacteriaCounts", individualBacteria, bacteriaLayout);
-    Plotly.newPlot("bubbleChart", individualBubble, bubbleLayout);
 
-    console.log(rawData);
+    let metadataTrace = [{
+        type: 'table',
+        header: {
+            values: [["Participant Demographics"]],
+            align: "center"
+        },
+        cells: {
+            values: [Object.keys(individualMetadata),Object.values(individualMetadata)],
+            align: "center",
+            font: {family: "Arial", size: 11, color: ["black"]}
+        }
+
+    }];
+
+    Plotly.newPlot("bacteriaCounts", bacteriaTrace, bacteriaLayout);
+    Plotly.newPlot("bubbleChart", bubbleTrace, bubbleLayout);
+    Plotly.newPlot("demographics", metadataTrace)
+
+    console.log(individualMetadata);
     console.log(individualMetadata);
 });
-
