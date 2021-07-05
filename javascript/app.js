5830c8e261dfcb5058ec1ef7f3dd1c7439a9e8ce
bakerv <80018640+bakerv@users.noreply.github.com>
2021-07-05 11:41:34 -0500

    added option tag to select menu with d3, need to do this with a for loop now

--- javascript/app.js
+++ javascript/app.js
@@ -6,6 +6,7 @@ let data = d3.json("data/samples.json").then((rawData) => {
     let lastElement = bacteria.sample_values.length -1;
 
     let individualMetadata = rawData.metadata[0];
+    let names = rawData.names
     
 
 
@@ -57,6 +58,38 @@ let data = d3.json("data/samples.json").then((rawData) => {
     Plotly.newPlot("bubbleChart", bubbleTrace, bubbleLayout);
     Plotly.newPlot("demographics", metadataTrace)
 
-    console.log(individualMetadata);
-    console.log(individualMetadata);
+    console.log(rawData);
+    console.log(names);
+
+
+    function displayData(){
+
+        // use d3 to set the cursor on specific html tags
+        let selectMenu = d3.select("#dataSet");
+        let dataOption = selectMenu.append("option")
+        // let thead = d3.select("thead");
+        // tbody.html("");
+        dataOption.text("dfdfdfdfdfdfdff");
+        dataOption.attr('value',"int");
+    };
+
+    displayData();
+        
+
 });
+
+        // Fill the data table using the values from each object in the data set
+        inputData.forEach((ufoReport) => {
+            let row = tbody.append("tr");
+            Object.entries(ufoReport).forEach(([key,value]) => {
+                let cell = row.append("td");
+                cell.text(value);
+            })
+        })
+
+        // Create table header using they keys from the data object
+        let header = thead.append("tr");
+        Object.entries(inputData[0]).forEach(([key,value]) => {
+            let contents = header.append("th");
+            contents.text(key);
+        })
