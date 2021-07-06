8ce39935bf385926fb37214a70d0fdc8a8dc7a9d
bakerv <80018640+bakerv@users.noreply.github.com>
2021-07-06 14:51:16 -0500

    page is fully operational. Plots all update via the select menu

--- javascript/app.js
+++ javascript/app.js
@@ -1,83 +1,89 @@
 let data = d3.json("data/samples.json").then((rawData) => {
-    
-    let bacteria = rawData.samples[0];
-    let lastElement = bacteria.sample_values.length -1;
+    function displayData(id) {
+        // extract information from the JSON
+        let bacteria = rawData.samples[id];
+        let lastElement = bacteria.sample_values.length -1;
+        let individualMetadata = rawData.metadata[id];
+        let names = rawData.names;
+        var x_value = [];
 
-    let individualMetadata = rawData.metadata[0];
-    let names = rawData.names
-    
-
-
-    // need to add interactive feature to adjust number of visible bars
-    let bacteriaTrace = [{
-        x: bacteria.sample_values.reverse().slice(lastElement-11,lastElement-1),
-
-        text: data.otu_labels,
-        type:"bar",
-        orientation:"h"
-    }];
-    //need to improve layout
-
-    let bacteriaLayout = {
-        title: "I hope this works!"
-    };
-
-    let bubbleTrace = [{
-
-        x:bacteria.otu_ids,
-        y: bacteria.sample_values,
-        text: bacteria.otu_labels,
-        mode: 'markers',
-        marker: {
-            size: bacteria.sample_values,
-            color: bacteria.otu_ids,
+        if (bacteria.sample_values.length > 10) {
+            x_value = bacteria.sample_values.reverse().slice(lastElement-11,lastElement-1);
         }
-    }];
-
-    let bubbleLayout = {
-        title: "This should also work"
-    };
-
-    let metadataTrace = [{
-        type: 'table',
-        header: {
-            values: [["Participant Demographics"]],
-            align: "center"
-        },
-        cells: {
-            values: [Object.keys(individualMetadata),Object.values(individualMetadata)],
-            align: "center",
-            font: {family: "Arial", size: 11, color: ["black"]}
+            else {
+            x_value = bacteria.sample_values.reverse();
         }
+        
+        // Set up parameters for Plotly
+        let bacteriaTrace = [{
+            x: x_value,
+            text: data.otu_labels,
+            type:"bar",
+            orientation:"h"
+        }];
 
-    }];
+        let bacteriaLayout = {
+            title: "I hope this works!"
+        };
 
-    Plotly.newPlot("bacteriaCounts", bacteriaTrace, bacteriaLayout);
-    Plotly.newPlot("bubbleChart", bubbleTrace, bubbleLayout);
-    Plotly.newPlot("demographics", metadataTrace)
+        let bubbleTrace = [{
+            x:bacteria.otu_ids,
+            y: bacteria.sample_values,
+            text: bacteria.otu_labels,
+            mode: 'markers',
+            marker: {
+                size: bacteria.sample_values,
+                color: bacteria.otu_ids,
+            }
+        }];
 
-    //console.log(rawData);
-    //console.log(names);
+        let bubbleLayout = {
+            title: "This should also work"
+        };
+
+        let metadataTrace = [{
+            type: 'table',
+            header: {
+                values: [["Participant Demographics"]],
+                align: "center"
+            },
+            cells: {
+                values: [Object.keys(individualMetadata),Object.values(individualMetadata)],
+                align: "center",
+                font: {family: "Arial", size: 11, color: ["black"]}
+            }
+        }];
+
+        Plotly.newPlot("bacteriaCounts", bacteriaTrace, bacteriaLayout);
+        Plotly.newPlot("bubbleChart", bubbleTrace, bubbleLayout);
+        Plotly.newPlot("demographics", metadataTrace)
+
+        //console.log(rawData);
+        //console.log(names);
 
 
-    function populateMenu(){
-        names.forEach((id,index) =>{
-            let selectMenu = d3.select("#dataSet");
-            let dataOption = selectMenu.append("option")
-            dataOption.text(id);
-            dataOption.attr('value',index);
-        })
+        function populateMenu(){
+            names.forEach((id,index) =>{
+                let selectMenu = d3.select("#dataSet");
+                let dataOption = selectMenu.append("option")
+                dataOption.text(id);
+                dataOption.attr('value',index);
+            })
+        };
+
+        populateMenu();
     };
 
     function selectData(){
        let selectMenu = d3.select("#dataSet");
        let dataSet = selectMenu.property("value");
-        console.log(dataSet)
+       console.log(dataSet);
+       displayData(dataSet);    
     };
         
+    displayData(0);
+    console.log(rawData.samples[133])
     
-    //selectData();
-    populateMenu();
 
     d3.selectAll("#dataSet").on("change", selectData);
 });
